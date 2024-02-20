42c1afe45c71848a49e9084918c6811434be985b
Lee SH <snghyun331@gmail.com>
2024-02-20 22:49:39 +0900

    feat: 사용자 인가 가드 생성

--- src/apis/diaries/diary.module.ts
+++ src/apis/diaries/diary.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DiaryEntity } from './entities/diary.entity';
+import { JwtStrategy } from 'src/auth/strategies/jwt.access.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([DiaryEntity])],
   controllers: [],
-  providers: [],
+  providers: [JwtStrategy],
 })
 export class DiaryModule {}
